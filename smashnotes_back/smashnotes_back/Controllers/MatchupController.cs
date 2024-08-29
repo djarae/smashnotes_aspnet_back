bff42194b943fcc1179ff279daacb8e789bb1ab0
djarae <djaraestivill@gmail.com>
2024-08-29 00:52:17 -0400

    Controller e integracion ok
    ok

---
+++ smashnotes_back/smashnotes_back/Controllers/MatchupController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace smashnotes_back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class MatchupController : ControllerBase
+    {
+        [HttpGet(Name = "GetMatchup")]
+        public string Get()
+        {
+            return "asdsa";
+        }
+
+    }
+}
---
+++ smashnotes_back/smashnotes_back/Program.cs
@@ -0,0 +1,47 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Correccion problema cors!
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica", app =>
+    {
+        app.AllowAnyOrigin().
+        AllowAnyHeader().
+        AllowAnyMethod();
+
+    }
+    );
+
+}
+);
+
+
+var app = builder.Build();
+
+// Correccion problema cors!
+app.UseCors("NuevaPolitica");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+
+
+app.Run();
